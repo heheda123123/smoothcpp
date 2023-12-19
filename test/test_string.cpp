b4aec3fe44a67d825fdc9526fb717935ce8ae099
cat <wacshuiguo@163.com>
2023-12-19 11:41:31 +0800

    unify use \n replace std::endl

--- test/test_string.cpp
+++ test/test_string.cpp
@@ -9,15 +9,15 @@ int main()
     std::cout << to_lower("aAaAAbb") << "\n";
     std::cout << to_upper("aaaAAbb") << "\n";
 
-    std::cout << ltrim("hello world", "eh") << std::endl;
-    std::cout << rtrim("hello world", "drl") << std::endl;
-    std::cout << trim("hello world", "held") << std::endl;
+    std::cout << ltrim("hello world", "eh") << "\n";
+    std::cout << rtrim("hello world", "drl") << "\n";
+    std::cout << trim("hello world", "held") << "\n";
 
     auto a = split_str("name=%s age=%d aaaaaaaaa", "=%");
-    std::cout << to_string(a) << std::endl;
+    std::cout << to_string(a) << "\n";
 
     auto b = split_chars("name=%s age=%d aaaaaaaaa", "=s");
-    std::cout << to_string(b) << std::endl;
+    std::cout << to_string(b) << "\n";
 
     auto c = split_str("aaaa bbbb cccc");
     auto d = join(b, " ");
@@ -26,16 +26,16 @@ int main()
     std::vector<int> testformat = {1, 2, 34};
     std::cout << to_string(testformat) << "\n";
 
-    std::cout << has_prefix("jjjaaa", "jj") << std::endl;
-    std::cout << has_suffix("jjjaaa", "jj") << std::endl;
+    std::cout << has_prefix("jjjaaa", "jj") << "\n";
+    std::cout << has_suffix("jjjaaa", "jj") << "\n";
 
     
     std::cout << contains("jjjjzzzbbb", "zzz") << "\n";
     
-    std::cout << format("name=%s age=%d", "jack", 18) << std::endl;
+    std::cout << format("name=%s age=%d", "jack", 18) << "\n";
 
     auto amap = std::map < std::string, int> {{"1", 2}, {"3", 4}, {"5", 6}};
-    std::cout << to_string(amap) << std::endl;
+    std::cout << to_string(amap) << "\n";
 
     return 0;
 }

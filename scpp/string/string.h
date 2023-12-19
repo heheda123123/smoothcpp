e151d6908f89220403d354e75d7f8889cd18532a
cat <wacshuiguo@163.com>
2023-12-19 11:26:29 +0800

    rename format to to_string to match std

--- scpp/string/string.h
+++ scpp/string/string.h
@@ -7,73 +7,73 @@
 
 namespace scpp
 {
-	std::string to_lower(std::string input);
-	std::string to_upper(std::string input);
+    std::string to_lower(std::string input);
+    std::string to_upper(std::string input);
 
-	std::string ltrim(std::string_view input, std::string_view trims = " ");
-	std::string rtrim(std::string_view input, std::string_view trims = " ");
-	std::string trim(std::string_view input, std::string_view trims = " ");
+    std::string ltrim(std::string_view input, std::string_view trims = " ");
+    std::string rtrim(std::string_view input, std::string_view trims = " ");
+    std::string trim(std::string_view input, std::string_view trims = " ");
 
-	std::vector<std::string> split_str(const std::string & input, const std::string & separator = " ");
-	std::vector<std::string> split_chars(const std::string & input, const std::string & separator = " ");
+    std::vector<std::string> split_str(const std::string & input, const std::string & separator = " ");
+    std::vector<std::string> split_chars(const std::string & input, const std::string & separator = " ");
 
-	std::string join(const std::vector<std::string> &input, std::string_view separator);
+    std::string join(const std::vector<std::string> &input, std::string_view separator);
 
-	bool has_prefix(std::string_view input, std::string_view prefix);
-	bool has_suffix(std::string_view input, std::string_view suffix);
-	bool contains(std::string_view str, std::string_view substr);
+    bool has_prefix(std::string_view input, std::string_view prefix);
+    bool has_suffix(std::string_view input, std::string_view suffix);
+    bool contains(std::string_view str, std::string_view substr);
 
-	std::string format(const char *format,...);
+    std::string format(const char *format,...);
 
-	template<typename T>
-	std::string format(const std::vector<T> v) {
-		std::stringstream ss;
-		ss << "[";
-		for (auto it = v.cbegin(); it != v.cend(); ++it) {
-			if (it == v.cbegin()) {
-				if (std::is_same<T, std::string>::value)
-					ss << "\"" << *it << "\"";
-				else
-					ss << *it;
-			} else {
-				if (std::is_same<T, std::string>::value)
-					ss << ", " << "\"" << *it << "\"";
-				else
-					ss << ", " << *it;
-			}
-		}
-		ss << "]";
-		return ss.str();
-	}
+    template<typename T>
+    std::string to_string(const std::vector<T> v) {
+        std::stringstream ss;
+        ss << "[";
+        for (auto it = v.cbegin(); it != v.cend(); ++it) {
+            if (it == v.cbegin()) {
+                if (std::is_same<T, std::string>::value)
+                    ss << "\"" << *it << "\"";
+                else
+                    ss << *it;
+            } else {
+                if (std::is_same<T, std::string>::value)
+                    ss << ", " << "\"" << *it << "\"";
+                else
+                    ss << ", " << *it;
+            }
+        }
+        ss << "]";
+        return ss.str();
+    }
 
-	template<typename T1, typename T2>
-	std::string format(const std::map<T1, T2> m) {
-		std::stringstream ss;
-		ss << "[";
-		for (auto it = m.cbegin(); it != m.cend(); ++it) {
-			if (it == m.cbegin()) {
-				if (std::is_same<T1, std::string>::value)
-					ss << "\"" << it->first << "\"";
-				else
-					ss << it->first;
-				ss << ": ";
-				if (std::is_same<T2, std::string>::value)
-					ss << "\"" << it->second << "\"";
-				else
-					ss << it->second;
-			} else {
-				if (std::is_same<T1, std::string>::value)
-					ss << ", " << "\"" << it->first << "\"";
-				else
-					ss << ", " << it->first;
-				ss << ":";
-				if (std::is_same<T2, std::string>::value)
-					ss << "\"" << it->second << "\"";
-				else
-					ss << it->second;
-			}
-		}
-		ss << "]";
-		return ss.str();
-	}
+    template<typename T1, typename T2>
+    std::string to_string(const std::map<T1, T2> m) {
+        std::stringstream ss;
+        ss << "[";
+        for (auto it = m.cbegin(); it != m.cend(); ++it) {
+            if (it == m.cbegin()) {
+                if (std::is_same<T1, std::string>::value)
+                    ss << "\"" << it->first << "\"";
+                else
+                    ss << it->first;
+                ss << ": ";
+                if (std::is_same<T2, std::string>::value)
+                    ss << "\"" << it->second << "\"";
+                else
+                    ss << it->second;
+            } else {
+                if (std::is_same<T1, std::string>::value)
+                    ss << ", " << "\"" << it->first << "\"";
+                else
+                    ss << ", " << it->first;
+                ss << ":";
+                if (std::is_same<T2, std::string>::value)
+                    ss << "\"" << it->second << "\"";
+                else
+                    ss << it->second;
+            }
+        }
+        ss << "]";
+        return ss.str();
+    }
 }
--- test/test_string.cpp
+++ test/test_string.cpp
@@ -6,36 +6,36 @@ using namespace scpp;
 
 int main()
 {
-	std::cout << to_lower("aaaAAbb") << "\n";
-	std::cout << to_upper("aaaAAbb") << "\n";
+    std::cout << to_lower("aaaAAbb") << "\n";
+    std::cout << to_upper("aaaAAbb") << "\n";
 
-	std::cout << ltrim("hello world", "eh") << std::endl;
-	std::cout << rtrim("hello world", "drl") << std::endl;
-	std::cout << trim("hello world", "held") << std::endl;
+    std::cout << ltrim("hello world", "eh") << std::endl;
+    std::cout << rtrim("hello world", "drl") << std::endl;
+    std::cout << trim("hello world", "held") << std::endl;
 
-	auto a = split_str("name=%s age=%d aaaaaaaaa", "=%");
-	std::cout << format(a) << std::endl;
+    auto a = split_str("name=%s age=%d aaaaaaaaa", "=%");
+    std::cout << to_string(a) << std::endl;
 
-	auto b = split_chars("name=%s age=%d aaaaaaaaa", "=s");
-	std::cout << format(b) << std::endl;
+    auto b = split_chars("name=%s age=%d aaaaaaaaa", "=s");
+    std::cout << to_string(b) << std::endl;
 
-	auto c = split_str("aaaa bbbb cccc");
-	auto d = join(b, " ");
-	std::cout << "c " << format(c) << "\n";
+    auto c = split_str("aaaa bbbb cccc");
+    auto d = join(b, " ");
+    std::cout << "c " << to_string(c) << "\n";
 
-	std::vector<int> testformat = {1, 2, 34};
-	std::cout << format(testformat) << "\n";
+    std::vector<int> testformat = {1, 2, 34};
+    std::cout << to_string(testformat) << "\n";
 
-	std::cout << has_prefix("jjjaaa", "jj") << std::endl;
-	std::cout << has_suffix("jjjaaa", "jj") << std::endl;
+    std::cout << has_prefix("jjjaaa", "jj") << std::endl;
+    std::cout << has_suffix("jjjaaa", "jj") << std::endl;
 
-	
-	std::cout << contains("jjjjzzzbbb", "zzz") << "\n";
-	
-	std::cout << format("name=%s age=%d", "jack", 18) << std::endl;
+    
+    std::cout << contains("jjjjzzzbbb", "zzz") << "\n";
+    
+    std::cout << format("name=%s age=%d", "jack", 18) << std::endl;
 
-	auto amap = std::map < std::string, int> {{"1", 2}, {"3", 4}, {"5", 6}};
-	std::cout << format(amap) << std::endl;
+    auto amap = std::map < std::string, int> {{"1", 2}, {"3", 4}, {"5", 6}};
+    std::cout << to_string(amap) << std::endl;
 
-	return 0;
+    return 0;
 }
